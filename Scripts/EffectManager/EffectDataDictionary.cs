3d6216e33d973bb39d9d37e9cc00e04769c13e33
ayaha401 <oshizawadai.1192@gmail.com>
2022-04-28 14:10:02 +0900

    スクリプト追加、パッケージ追加

--- Scripts/EffectManager/EffectDataDictionary.cs
+++ Scripts/EffectManager/EffectDataDictionary.cs
@@ -7,6 +7,37 @@ namespace Effect
     public class EffectDataDictionary : MonoBehaviour
     {
         [SerializeField] private EffectListSObj _effectListSObj = null;
+
+        private Dictionary<string, GameObject> _effectDic = null;
+
+        // ディクショナリを作成
+        private void MakeDictionary()
+        {
+            _effectDic = new Dictionary<string, GameObject>();
+            foreach (GameObject effectObj in _effectListSObj.effectDatas)
+            {
+                if (_effectDic.ContainsKey(effectObj.name) == false)
+                {
+                    _effectDic.Add(effectObj.name, effectObj);
+                }
+            }
+        }
+
+        private void Awake()
+        {
+            MakeDictionary();
+        }
+
+        // ディクショナリを返す
+        public Dictionary<string, GameObject> GetEffectDic()
+        {
+            if(_effectDic == null)
+            {
+                MakeDictionary();
+            }
+
+            return _effectDic;
+        }
     }
 }
 
--- Scripts/EffectManager/EffectManager.cs
+++ Scripts/EffectManager/EffectManager.cs
@@ -6,10 +6,8 @@ namespace Effect
 {
     public class EffectManager : MonoBehaviour
     {
-        //[SerializeField] GameObject obj;
         private static GameObject _parentObj = null;
         
-
         private void Awake()
         {
             _parentObj = this.gameObject;
@@ -20,16 +18,43 @@ namespace Effect
             
         }
 
-        void Update()
+        // ParticleSystemを設定
+        private static void ParticleSystemSetting(GameObject effectObj)
         {
-            
+            if(effectObj.GetComponent<ParticleSystem>() == null) return;
+
+            ParticleSystem.MainModule particleSystem = effectObj.GetComponent<ParticleSystem>().main;
+            particleSystem.stopAction = ParticleSystemStopAction.Disable;
         }
 
-        public static void EffectPlay(string effectName, Vector2 pos)
+        // ディクショナリからエフェクトを取得
+        private static void GetEffect(EffectDataDictionary dataDic, string effectName, Vector2 pos, GameObject rootObj)
         {
-            GameObject effectObj = new GameObject("newObj");
-            //Instantiate(effectObj, pos, Quaternion.identity);
-            effectObj.transform.parent = _parentObj.transform;
+            Dictionary<string, GameObject> effectDic = dataDic.GetEffectDic();
+            if (effectDic.ContainsKey(effectName) == false)
+            {
+                Debug.Log("対応するstring : " + effectName + "がありません");
+                return;
+            }
+
+            GameObject effectObj = effectDic[effectName];
+
+            ParticleSystemSetting(effectObj);
+
+            effectObj = Instantiate(effectObj, pos, Quaternion.identity);
+            effectObj.transform.parent = rootObj.transform;
+        }
+
+        // 指定座標にエフェクトを発生
+        public static void EffectPlay(EffectDataDictionary dataDic, string effectName, Vector2 pos)
+        {
+            GetEffect(dataDic, effectName, pos, _parentObj);
+        }
+
+        // し指定オブジェクトの子オブジェクトとしてエフェクトを発生
+        public static void EffectTrackingPlay(EffectDataDictionary dataDic, string effectName, Vector2 pos, GameObject rootObj)
+        {
+            GetEffect(dataDic, effectName, pos, rootObj);
         }
     }
 }
